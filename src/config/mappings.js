12d127d1a74db971a593d0ea5712ab68d777e32d
Biruk <kassayebiruk0@gmail.com>
2025-10-28 14:47:06 +0300

    fix: add missing user to the mapping

--- src/config/mappings.js
+++ src/config/mappings.js
@@ -1,6 +1,14 @@
 export const clickupToSlack = {
   "Biruk Kassaye": "U07GEC2GC85",
   "Leul B": "U07H62GM264",
+  "Getachew Deriba": "U08UGGP7M6V",
+  "John Merga": "U06NMSSAVP1",
+  Kirubel: "U077F4MQB9B",
+  "Melaku Ftwi": "U04D5UG38MS",
+  "Milkias Tonji": "U06JVUJGFHN",
+  "Nahom Tensabirhan": "U08RR4M7SB0",
+  "Yohannes Mekonnen": "U092FGE5540",
+  Zemelat: "U06NFN1UAKE",
 };
 
 export const relevantStatuses = [
