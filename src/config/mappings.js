a68187d761960a5eb4dab16565509220361e9bc9
Biruk <kassayebiruk0@gmail.com>
2025-10-30 18:09:53 +0300

    update the mapping

--- src/config/mappings.js
+++ src/config/mappings.js
@@ -1,15 +1,14 @@
 export const clickupToSlack = {
-  "Biruk Kassaye": "U07GEC2GC85",
-  // "Leul B": "U07GEC2GC85",
   "Leul B": "U07H62GM264",
-  // "Getachew Deriba": "U08UGGP7M6V",
-  // "John Merga": "U06NMSSAVP1",
-  // Kirubel: "U077F4MQB9B",
-  // "Melaku Ftwi": "U04D5UG38MS",
-  // "Milkias Tonji": "U06JVUJGFHN",
-  // "Nahom Tensabirhan": "U08RR4M7SB0",
-  // "Yohannes Mekonnen": "U092FGE5540",
-  // Zemelat: "U06NFN1UAKE",
+  "Getachew Deriba": "U08UGGP7M6V",
+  "John Merga": "U06NMSSAVP1",
+  Kirubel: "U077F4MQB9B",
+  "Melaku Ftwi": "U04D5UG38MS",
+  "Milkias Tonji": "U06JVUJGFHN",
+  "Nahom Tensabirhan": "U08RR4M7SB0",
+  "Yohannes Mekonnen": "U092FGE5540",
+  Zemelat: "U06NFN1UAKE",
+  // "Biruk Abate": "U072X73MH7H",
 };
 
 export const relevantStatuses = [
