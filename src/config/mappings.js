e466ccdacf32a7812aa11cb880b2a94d4dcc63cb
Biruk <kassayebiruk0@gmail.com>
2025-11-05 16:44:09 +0300

    fix: correct the bug in mapping

--- src/config/mappings.js
+++ src/config/mappings.js
@@ -1,11 +1,11 @@
 export const clickupToSlack = {
   "Leul B": "U07H62GM264",
-  "Getachew Deriba": "U08UGGP7M6V",
+  "Getachew Derib": "U08UGGP7M6V",
   "John Merga": "U06NMSSAVP1",
   Kirubel: "U077F4MQB9B",
   "Melaku Ftwi": "U04D5UG38MS",
   "Milkias Tonji": "U06JVUJGFHN",
-  "Nahom Tensabirhan": "U08RR4M7SB0",
+  "Nahom Tensaebirhan": "U08RR4M7SB0",
   "Yohannes Mekonnen": "U092FGE5540",
   Zemelat: "U06NFN1UAKE",
   // "Biruk Abate": "U072X73MH7H",
@@ -19,3 +19,4 @@ export const relevantStatuses = [
 ];
 
 export const trackedSpaces = ["Poker Real Gaming & Store"];
+export const projectManagers = ["Melaku Ftwi", "Biruk Abate"];
